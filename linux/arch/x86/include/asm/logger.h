5025f1f4919bf000d90787e6cf4435a46bd5d33b
Le Tan <tamlokveer@gmail.com>
2015-10-05 02:02:46 +0000

    logger: add RR_ASSERT() for assertion
    Add RR_ASSERT() for assertion if DEBUG_RECORD_REPLAY is defined.

--- linux/arch/x86/include/asm/logger.h
+++ linux/arch/x86/include/asm/logger.h
@@ -1,5 +1,7 @@
 #ifndef _ASM_X86_LOGGER_H
 #define _ASM_X86_LOGGER_H
+#include <linux/printk.h>
+#include <asm/bug.h>
 
 extern int rr_log(const char* fmt, ...);
 
@@ -17,8 +19,18 @@ static int rr_dbflags = RR_DBBIT(GEN) | RR_DBBIT(ERR) | RR_DBBIT(INIT);
 		rr_log("%s: " fmt "\n", __func__, \
 		       ## __VA_ARGS__); } \
 	} while (0)
+
+#define RR_ASSERT(x) do { \
+	if (unlikely(!(x))) { \
+		pr_err("error: rr assertion failed! %s, %s, %s #%d\n", \
+		       #x, __FILE__, __func__, __LINE__); \
+		BUG(); } \
+	} while (0)
+
 #else
 #define RR_DLOG(what, fmt, ...) do {} while (0)
+#define RR_ASSERT(x) do {} while (0)
 #endif
 
 #endif
+
